a3359f052fee94894280c719383263efe3efde11
fenrissa92 <178785253+fenrissa92@users.noreply.github.com>
2024-12-03 21:27:53 +0000

    task 1

---
+++ main.js
@@ -0,0 +1,48 @@
+/* Task 1 - Declaring Objects
+
+In your `main.js`, convert the following sentences into JavaScript objects.
+
+> My cat is black, his name is Tony, and he loves cuddles.
+
+```js
+const myCat = {
+  color: "black",
+  name: "Tony",
+  lovesCuddles: true,
+};
+```
+
+👉 Convert the rest!
+
+Create an object called `myHouse`:
+> My house has 2 stories, no parking, 2 bedrooms, a garden and an energy efficiency rating of D.
+
+Create an object called `myCar`:
+> My car is red, has 4 wheels, power steering, and 2 seats, and does 20 miles per gallon.
+
+Create an object called `myFavouriteFilm`:
+> My favourite film's title is The Dark Knight, it was released in 2008, and the lead actor is Christian Bale.
+
+*/
+
+const myHouse = {
+    numberOfFloors: 2,
+    hasParking: false,
+    numberOfBedrooms: 2,
+    hasGarden: true,
+    energyRating: "D",
+};
+
+const myCar = {
+    colour: "red",
+    wheelsNumber: 4,
+    steeringType: "power",
+    numberOfSeats: 2,
+    milesPerGalon: 20,
+};
+
+const myFavouriteFilm = {
+    title: "The Dark Knight",
+    releasedDate: 2008,
+    leadActor: "Christian Bale",
+};
